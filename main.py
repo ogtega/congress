4b8fd90078a113fc1d579c6bb572c46c95c90af6
Teslim Olunlade <tolunlade@outlook.com>
2020-11-07 04:25:41 -0500

    Made all function return lists

--- main.py
+++ main.py
@@ -27,10 +27,10 @@ headers: Dict[str, str] = {
 
 
 def main():
-    # fetch_states()
-    # fetch_cds()
-    # fetch_senate()
-    # fetch_house()
+    # print(fetch_states())
+    # print(fetch_cds())
+    # print(fetch_senate())
+    # print(fetch_house())
     fetch_bills()
     # TODO: Get bill status (hr, s) and resolutions (hjres, sjres) from https://www.govinfo.gov/bulkdata/json/BILLSTATUS/116
 
@@ -70,6 +70,7 @@ def fetch_bills():
 
 
 def parse_bill(file):
+    bill: Dict[str, str] = dict()
     parser = ET.iterparse(file, events=("start", "end"))
 
     event: str
@@ -85,16 +86,20 @@ def fetch_states() -> List[Dict[str, str]]:
     reader = csv.DictReader(codecs.iterdecode(file, "utf-8"), delimiter="|")
 
     for row in reader:
-        fips = row["STATE"]
-        abrev = row["STUSAB"].lower()
-        name = row["STATE_NAME"]
-        states.append({"_id": abrev, "fips": fips, "name": name})
-    file.close()
+        states.append(
+            {
+                "id": row["STUSAB"].lower(),
+                "fips": row["STATE"],
+                "name": row["STATE_NAME"],
+            }
+        )
 
+    file.close()
     return states
 
 
 def fetch_cds():
+    cds = list()
     ftp = FTP("ftp.census.gov")
     ftp.login()
 
@@ -105,12 +110,26 @@ def fetch_cds():
             file = download(
                 "https://www2.census.gov/{}".format(files[0]), headers, ".zip"
             )
-            shapefile_to_json("zip://{}".format(file.name))
+
+            source: Collection
+            with fiona.open("zip://{}".format(file.name)) as source:
+                f: Dict[str, Any]
+                for f in source:
+                    cds.append(
+                        {
+                            "id": f["properties"]["GEOID"],
+                            "statefp": f["properties"]["STATEFP"],
+                            "district": f["properties"]["GEOID"][-2:],
+                            "geometry": f["geometry"],
+                        }
+                    )
             file.close()
             break
+    return cds
 
 
 def fetch_house():
+    members: List[Dict[str, str]] = list()
     file = download("https://clerk.house.gov/xml/lists/MemberData.xml", headers)
 
     parser = ET.iterparse(file.name, events=("start", "end"))
@@ -121,24 +140,23 @@ def fetch_house():
         if event == "end" and elem.tag == "member":
             info = elem.find("member-info")
 
-            fname = info.find("firstname").text
-            lname = info.find("lastname").text
-            party = info.find("party").text
-            state = info.find("state").attrib["postal-code"]
-            district = elem.find("statedistrict").text
-            bioguide = info.find("bioguideID").text
-
-            print(
-                "{{{}, {}, {}, {}, {{{}, {}}}".format(
-                    bioguide, fname, lname, party, state, district
-                )
+            members.append(
+                {
+                    "id": info.find("bioguideID").text,
+                    "fname": info.find("firstname").text,
+                    "lname": info.find("lastname").text,
+                    "party": info.find("party").text,
+                    "state": info.find("state").attrib["postal-code"],
+                    "district": elem.find("statedistrict").text,
+                }
             )
 
-            elem.clear()
     file.close()
+    return members
 
 
 def fetch_senate():
+    members: List[Dict[str, str]] = list()
     file = download(
         "https://www.senate.gov/general/contact_information/senators_cfm.xml", headers
     )
@@ -149,21 +167,19 @@ def fetch_senate():
     elem: ET.Element
     for event, elem in parser:
         if event == "end" and elem.tag == "member":
-            fname = elem.find("first_name").text
-            lname = elem.find("last_name").text
-            party = elem.find("party").text
-            state = elem.find("state").text
-            bioguide = elem.find("bioguide_id").text
-            _class = elem.find("class").text
-
-            print(
-                "{{{}, {}, {}, {}, {{{}, {}}}}}".format(
-                    bioguide, fname, lname, party, state, _class
-                )
+            members.append(
+                {
+                    "id": elem.find("bioguide_id").text,
+                    "fname": elem.find("first_name").text,
+                    "lname": elem.find("last_name").text,
+                    "party": elem.find("party").text,
+                    "state": elem.find("state").text,
+                    "class": elem.find("class").text,
+                }
             )
 
-            elem.clear()
     file.close()
+    return members
 
 
 def download(url: str, headers: Dict[str, str] = dict(), suffix: str = "") -> IO[bytes]:
@@ -181,26 +197,6 @@ def download(url: str, headers: Dict[str, str] = dict(), suffix: str = "") -> IO
 def gen_congress(year=datetime.today().year) -> Generator[Tuple[int, int], None, None]:
     for i in range(year, 1788, -1):
         yield (i, math.floor((i + 1) / 2 - 894))
-
-
-def shapefile_to_json(file):
-    geojson = []
-
-    source: Collection
-    with fiona.open(file) as source:
-        f: Dict[str, Any]
-        for f in source:
-            geojson.append(
-                {
-                    "id": f["properties"]["GEOID"],
-                    "statefp": f["properties"]["STATEFP"],
-                    "district": f["properties"]["GEOID"][-2:],
-                    "geometry": f["geometry"],
-                }
-            )
-
-    with open(r"shape.json", "w") as file:
-        json.dump(geojson, file, indent=4)
 
 
 if __name__ == "__main__":
