0c4e15d72bc5883f902abbbe29eabef176d9ad74
Teslim Olunlade <tolunlade@outlook.com>
2020-11-24 03:58:24 -0500

    Changed bills output file for sample

--- samples/fetch_bills.py
+++ samples/fetch_bills.py
@@ -7,7 +7,7 @@ from congress import fetch_bills, get_congress
 
 def main():
     congress = get_congress()
-    with open("congress.json", "w") as f:
+    with open("bills.json", "w") as f:
         json.dump(fetch_bills(congress), f, indent=4, sort_keys=True)
 
 
