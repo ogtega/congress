3095d47f146f144382b5b2abd20e88c1805b38c0
Teslim Olunlade <tolunlade@outlook.com>
2020-11-25 04:46:18 -0500

    Organized imports

--- congress/__init__.py
+++ congress/__init__.py
@@ -1,7 +1,7 @@
 from .bills import fetch_bills
 from .districts import fetch_districts
 from .members import fetch_house, fetch_senate
-from .utils import get_congress, gen_congress
+from .utils import gen_congress, get_congress
 
 del bills
 del districts
