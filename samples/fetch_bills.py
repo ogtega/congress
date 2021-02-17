ba8ec13af6bb9b148b2ac669a64239c6f8f1e769
Teslim Olunlade <tolunlade@outlook.com>
2021-02-17 08:54:09 -0500

    Updated sample scripts

--- samples/fetch_bills.py
+++ samples/fetch_bills.py
@@ -4,7 +4,6 @@ import json
 import os
 
 from congress import fetch_bills
-import congress
 
 
 def main():
@@ -14,15 +13,20 @@ def main():
         fname = f"bills/{bill.id}.json"
         os.makedirs(os.path.dirname(fname), exist_ok=True)
         with open(fname, "w") as f:
-            json.dump({
-                'id': bill.id,
-                'title': bill.title,
-                'congress': bill.congress,
-                'summary': bill.summary,
-                'subjects': bill.subjects,
-                'sponsors': bill.sponsors,
-                'cosponsors': bill.cosponsors
-            }, f, indent=4, sort_keys=True)
+            json.dump(
+                {
+                    "id": bill.id,
+                    "title": bill.title,
+                    "congress": bill.congress,
+                    "summary": bill.summary,
+                    "subjects": bill.subjects,
+                    "sponsors": bill.sponsors,
+                    "cosponsors": bill.cosponsors,
+                },
+                f,
+                indent=4,
+                sort_keys=True,
+            )
 
 
 if __name__ == "__main__":
